f7a8d0617d19126817b291b34357aaa2b18cea2d
Scott Pessetto <scott.pessetto@gmail.com>
2024-05-09 15:01:19 -0800

    first commit

---
+++ process_forecast.py
@@ -0,0 +1,56 @@
+from datetime import timedelta
+from typing import Any
+
+import pandas as pd
+
+
+def process_well_data(excel_file: str) -> pd.DataFrame:
+    """
+    Processes well data from an Excel file to calculate and append a new date column based on start dates and month offsets.
+
+    Args:
+        excel_file (str): The path to the Excel file containing the well forecasts and start dates
+
+    Returns:
+        pd.DataFrame: The forecast DataFrame with an additional 'Date' column containing the calculated dates for
+        each month of the forecast.
+
+    This function loads data from two sheets in the specified Excel file: 'start_dates' and 'forecast'.
+    It maps well names to their respective start dates, calculates new dates by adding a month offset to these start dates,
+    and appends these dates to the 'forecast' DataFrame.
+    """
+    # Load the workbook and the specific sheets
+    print("Loading start dates")
+    start_dates_sheet = pd.read_excel(excel_file, sheet_name="start_dates", skiprows=1)
+    print("Loading forecasts")
+    forecast_sheet = pd.read_excel(excel_file, sheet_name="forecast", skiprows=1)
+
+    # Convert date columns to datetime
+    start_dates_sheet["Start Date"] = pd.to_datetime(start_dates_sheet["Forecast Start Date"]).dt.normalize()
+
+    # Create a dictionary to map well names to start dates
+    print("Creating start dict")
+    start_date_dict = dict(zip(start_dates_sheet["Entity"], start_dates_sheet["Start Date"]))
+
+    # Initialize a new 'Date' column in forecast_sheet with NaN or a similar placeholder
+    forecast_sheet["Date"] = pd.NaT
+
+    # Process the forecast data
+    for index, row in forecast_sheet.iterrows():
+        well_name = row["Entity"]
+        print(well_name)
+        if well_name in start_date_dict:
+            start_date = start_date_dict[well_name]
+
+            month_offset = pd.DateOffset(months=row["Month"] - 1)
+            calculated_date = start_date + month_offset
+            calculated_date = calculated_date.replace(day=1)
+            forecast_sheet.at[index, "Date"] = calculated_date
+
+    return forecast_sheet
+
+
+# Usage
+result = process_well_data("Forecast Inputs Template - April 2024 with H and J Development graphed temp.xlsx")
+print(result)
+result.to_csv("forecast_withdate.csv")
---
+++ viewer.py
@@ -0,0 +1,107 @@
+from datetime import timedelta
+
+import altair as alt
+import pandas as pd
+import streamlit as st
+
+# run by running 'streamlit run viewer.py' in the command prompt
+
+
+def wide_space_default():
+    st.set_page_config(layout="wide")
+
+
+wide_space_default()
+st.title("Forecast Viewer")
+
+
+# Load data
+@st.cache_data
+def load_data():
+    data = pd.read_csv("forecast_withdate.csv")
+    data["Date"] = pd.to_datetime(data["Date"])
+    return data
+
+
+df = load_data()
+
+
+default_entity = sorted(df["Entity"].unique())  # Sort the entity list alphabetically
+default_entity_selection = [default_entity[0]] if default_entity else []
+
+
+# default_start_date = df["Date"].min()
+# default_end_date = default_start_date + timedelta(days=1000)
+default_start_date = df["Date"].min().to_pydatetime()
+default_end_date = df["Date"].max().to_pydatetime()
+date_range = [default_start_date, default_end_date]
+
+entity = st.sidebar.multiselect(
+    "Select Entity (Push x to see all):", options=default_entity, default=default_entity_selection
+)
+# date_range = st.sidebar.date_input("Select Date Range:", value=date_range)
+
+date_range = st.sidebar.slider(
+    "Select Date Range:",
+    min_value=default_start_date,
+    max_value=default_end_date,
+    value=(default_start_date, default_end_date),
+)
+# Convert date_range to pandas timestamps
+date_range = pd.to_datetime(date_range)
+likelihood = st.sidebar.multiselect("Select Likelihood", options=df["Likelihood"].unique(), default="Mid")
+
+
+# Function to filter data based on selection
+def filter_data(df, entity, date_range) -> pd.DataFrame:
+    """
+    Filters the data based on the selected entity, date range, and likelihood.
+
+    Args:
+    df (pd.DataFrame): The original DataFrame.
+    entity (list): List of selected entities.
+    date_range (tuple): Tuple containing the start and end date.
+    likelihood (list): List of selected likelihoods.
+
+    Returns:
+    pd.DataFrame: The filtered DataFrame.
+    """
+    if entity:
+        df = df[df["Entity"].isin(entity)]
+    if not date_range.empty:
+        df = df[(df["Date"] >= date_range[0]) & (df["Date"] <= date_range[1])]
+    if likelihood:
+        df = df[df["Likelihood"].isin(likelihood)]
+    return df
+
+
+def plot_stacked_area(df, rate_stream):
+    if df.empty:
+        return None
+    chart = (
+        alt.Chart(df)
+        .mark_area()
+        .encode(
+            x="Date:T",
+            y=alt.Y(f"{rate_stream}:Q", stack=True),
+            color="Entity:N",
+            tooltip=["Entity", rate_stream, "Date"],
+        )
+        .interactive()
+        .properties(width=2500, height=1000)
+    )
+    return chart
+
+
+filtered_df = filter_data(df, entity, date_range)
+
+
+if not filtered_df.empty:
+    st.title("Oil Forecasts")
+    st.altair_chart(plot_stacked_area(filtered_df, "Oil Rate"), use_container_width=True)
+    st.title("Gas Forecasts")
+    st.altair_chart(plot_stacked_area(filtered_df, "Form Gas Rate"), use_container_width=True)
+    st.title("Water Forecasts")
+    st.altair_chart(plot_stacked_area(filtered_df, "Water Rate"), use_container_width=True)
+else:
+    st.write("No data to display. Please select different filters.")
